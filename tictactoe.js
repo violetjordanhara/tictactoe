6b25f4d72a900497c2b91590f93612f87ebcfd47
vi <vjordanhara@gmail.com>
2023-04-08 12:08:40 +0100

    added game flow

--- tictactoe.js
+++ tictactoe.js
@@ -5,8 +5,8 @@ const gameboard = (() => {
 
 const player = (playerNumber, markType) => {
     const placeMark = () => {
-        let row = prompt('which row');
-        let col = prompt('which column');
+        let row = prompt(`player ${playerNumber}, which row`);
+        let col = prompt(`player ${playerNumber}, which column`);
         gameboard.board[row][col] = `${markType}`
         console.log(gameboard.board)};
     return { playerNumber, markType, placeMark };
@@ -14,9 +14,51 @@ const player = (playerNumber, markType) => {
 
 const playerOne = player('one', 'x');
 const playerTwo = player('two', 'o');
-/*
+
 const gameflow = (() =>{
- 
+ //make sure each player gets a turn until someone wins
+ //how do i prevent overlaps
+ let totalNumberOfTurns = 0;
+ while (totalNumberOfTurns<9){
+ playerOne.placeMark();
+ totalNumberOfTurns++;
+ console.log(totalNumberOfTurns);
+    //winning conditions
+    //all of the rows 
+    const allEqualX = arr => arr.every( v => v === 'x' )
+    if (totalNumberOfTurns == 9){
+        console.log('stop game; tie')
+        break
+    }else if (allEqualX( gameboard.board[0] || gameboard.board[1] || gameboard.board[2]) == true){
+        console.log('Player one wins');
+        break
+        //all of the columns
+        //2 diagonals
+    }  else if ((gameboard.board[0][0] == gameboard.board[1][0] && gameboard.board[1][0] == gameboard.board[2][0] && gameboard.board[2][0] == 'x')||
+            (gameboard.board[0][1] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][1] && gameboard.board[2][1] =='x')||
+            (gameboard.board[0][2] == gameboard.board[1][2] && gameboard.board[1][2] == gameboard.board[2][2] && gameboard.board[2][2] =='x')||
+            (gameboard.board[0][0] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][2] && gameboard.board[2][2] == 'x')||
+            (gameboard.board[0][2] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][0] && gameboard.board[2][0] =='x')){
+        console.log('Player one wins');
+        break
     }
-})
-*/
+
+//playerTwo's turn
+    playerTwo.placeMark();
+    totalNumberOfTurns++;
+    console.log(totalNumberOfTurns);
+    const allEqualO = arr => arr.every( v => v === 'o' )
+    if (allEqualO( gameboard.board[0] || gameboard.board[1] || gameboard.board[2]) == true){
+        console.log('Player two wins');
+        break
+    }  else if ((gameboard.board[0][0] == gameboard.board[1][0] && gameboard.board[1][0] == gameboard.board[2][0] && gameboard.board[2][0] == 'o')||
+            (gameboard.board[0][1] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][1] && gameboard.board[2][1] =='o')||
+            (gameboard.board[0][2] == gameboard.board[1][2] && gameboard.board[1][2] == gameboard.board[2][2] && gameboard.board[2][2] =='o')||
+            (gameboard.board[0][0] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][2] && gameboard.board[2][2] == 'o')||
+            (gameboard.board[0][2] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][0] && gameboard.board[2][0] == 'o')){
+        console.log('Player two wins');
+        break
+    }
+ }
+    }
+)();
