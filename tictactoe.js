11d347d96ce8f1a56aca0e3aa69efc0030ea5bb6
vi <vjordanhara@gmail.com>
2023-04-10 18:42:12 +0100

    players can take turns to play

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,5 @@
 const gameboard = (() => {
-    let board = [['o', 'x', 'o'],['x', 'o', 'x'],['o', 'x', 'o']];
+    const board = [['', '', ''],['', '', ''],['', '', '']];
     return{ board }
 })();
 
@@ -17,73 +17,64 @@ const gameondom = (() =>{
         cell.innerText = gameboard.board[i][j];
         cell.id = `${i}-${j}`;
        }
+
+  
+    document.querySelectorAll('.grid-item').forEach(item =>{
+    item.addEventListener("click", function(e){
+        gameflow.gotClickedWhere(e.target.id)
+        })
+    })
   };
 })();
 
 
 
 const player = (playerNumber, markType) => {
-    const placeMark = () => {
-        //// add event listener to each cell. when each cell is clicked, that player's marktype is given to the cell's relative position in the array
-        //the cell that the player clicks updates the player mark to the position in the gameboard
-        document.querySelectorAll('.grid-item').forEach(item =>{
-        item.addEventListener('click', event => {
-        item.innerText = player.markType;
-        gameboard.board[item.id.charAt(0)][item.id.charAt(2)]= player.markType
-        console.log(gameboard.board)
-        })
-    })
-        console.log(gameboard.board)};
-    return { playerNumber, markType, placeMark };
+    return { playerNumber, markType};
 }
 
 const playerOne = player('one', 'x');
 const playerTwo = player('two', 'o');
 
-const gameflow = (() =>{
- //make sure each player gets a turn until someone wins
- //how do i prevent overlaps
- let totalNumberOfTurns = 0;
- while (totalNumberOfTurns<9){
- playerOne.placeMark();
- totalNumberOfTurns++;
- console.log(totalNumberOfTurns);
-    //winning conditions
-    //all of the rows 
-    const allEqualX = arr => arr.every( v => v === 'x' )
-    if (totalNumberOfTurns == 9){
-        console.log('stop game; tie')
-        break
-    }else if (allEqualX( gameboard.board[0] || gameboard.board[1] || gameboard.board[2]) == true){
-        console.log('Player one wins');
-        break
-        //all of the columns
-        //2 diagonals
-    }  else if ((gameboard.board[0][0] == gameboard.board[1][0] && gameboard.board[1][0] == gameboard.board[2][0] && gameboard.board[2][0] == 'x')||
-            (gameboard.board[0][1] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][1] && gameboard.board[2][1] =='x')||
-            (gameboard.board[0][2] == gameboard.board[1][2] && gameboard.board[1][2] == gameboard.board[2][2] && gameboard.board[2][2] =='x')||
-            (gameboard.board[0][0] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][2] && gameboard.board[2][2] == 'x')||
-            (gameboard.board[0][2] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][0] && gameboard.board[2][0] =='x')){
-        console.log('Player one wins');
-        break
+const gameflow = (() => {
+    let listofplayers = [playerOne, playerTwo]
+    let currentPlayer = playerOne;
+    let turncounter= 0;
+
+    const gotClickedWhere = (id) => {
+        if(gameboard.board[id.charAt(0)][id.charAt(2)] == ''){
+            gameboard.board[id.charAt(0)][id.charAt(2)]= currentPlayer.markType;
+            document.getElementById(`${id.charAt(0)}-${id.charAt(2)}`).innerHTML = currentPlayer.markType;
+
+            turncounter ++;
+
+            changeCurrentPlayer();
+            checkWin();
+        }
     }
 
-//playerTwo's turn
-    playerTwo.placeMark();
-    totalNumberOfTurns++;
-    console.log(totalNumberOfTurns);
-    const allEqualO = arr => arr.every( v => v === 'o' )
-    if (allEqualO( gameboard.board[0] || gameboard.board[1] || gameboard.board[2]) == true){
-        console.log('Player two wins');
-        break
-    }  else if ((gameboard.board[0][0] == gameboard.board[1][0] && gameboard.board[1][0] == gameboard.board[2][0] && gameboard.board[2][0] == 'o')||
-            (gameboard.board[0][1] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][1] && gameboard.board[2][1] =='o')||
-            (gameboard.board[0][2] == gameboard.board[1][2] && gameboard.board[1][2] == gameboard.board[2][2] && gameboard.board[2][2] =='o')||
-            (gameboard.board[0][0] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][2] && gameboard.board[2][2] == 'o')||
-            (gameboard.board[0][2] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][0] && gameboard.board[2][0] == 'o')){
-        console.log('Player two wins');
-        break
+    const changeCurrentPlayer = () => {
+        if(currentPlayer == playerOne){
+            currentPlayer = playerTwo;
+        }else{
+            currentPlayer = playerOne;
+        }
     }
- }
+
+    const checkWin = () => {
+        const allEqualX = arr => arr.every( v => v === player.markType );
+
+        if (turncounter == 9){
+        console.log('stop game; tie');
+        }else if (allEqualX( gameboard.board[0] || gameboard.board[1] || gameboard.board[2]) == true){
+        console.log(`${currentPlayer} wins`);
+        }else if ((gameboard.board[0][0] == gameboard.board[1][0] && gameboard.board[1][0] == gameboard.board[2][0] && gameboard.board[2][0] == currentPlayer.markType)||
+            (gameboard.board[0][1] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][1] && gameboard.board[2][1] == currentPlayer.markType)||
+            (gameboard.board[0][2] == gameboard.board[1][2] && gameboard.board[1][2] == gameboard.board[2][2] && gameboard.board[2][2] == currentPlayer.markType)||
+            (gameboard.board[0][0] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][2] && gameboard.board[2][2] == currentPlayer.markType)||
+            (gameboard.board[0][2] == gameboard.board[1][1] && gameboard.board[1][1] == gameboard.board[2][0] && gameboard.board[2][0] == currentPlayer.markType)){
+        console.log(`${playerNumber} wins`);
+        }
     }
-)();
+    return {gotClickedWhere, changeCurrentPlayer, checkWin}
+})();
